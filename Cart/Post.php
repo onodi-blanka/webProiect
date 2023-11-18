74e2bbc6f66b2557fe1988a19fea43f33b9254d2
blanka <blanka@DESKTOP-1UP747C>
2023-11-18 05:34:10 -0900

    Proiect web cart post (in legatura cu nr de bilete cand se face cumparare)

--- Cart/Post.php
+++ Cart/Post.php
@@ -10,16 +10,42 @@ if (isset($_POST['submit'])) {
     if ($UserID == '' || $EventID == '' || $NumberOfTickets == '') {
         $error = 'ERROR: Campuri goale!';
     } else {
-        if ($stmt = $mysqli->prepare("INSERT INTO cart (UserID, EventID, NumberOfTickets) VALUES (?, ?, ?)")) {
-            $stmt->bind_param("iii", $UserID, $EventID, $NumberOfTickets);
-            $stmt->execute();
-            $stmt->close();
-        } else {
-            echo "ERROR: Nu se poate executa insert.";
+        // Începeți tranzacția
+        $mysqli->begin_transaction();
+
+        try {
+            // Inserare în Cart
+            if ($stmt = $mysqli->prepare("INSERT INTO cart (UserID, EventID, NumberOfTickets) VALUES (?, ?, ?)")) {
+                $stmt->bind_param("iii", $UserID, $EventID, $NumberOfTickets);
+                $stmt->execute();
+                $stmt->close();
+            } else {
+                throw new Exception("Nu se poate executa insert.");
+            }
+
+            // Actualizare Event
+            if ($stmt = $mysqli->prepare("UPDATE event SET Tickets = Tickets - ? WHERE ID = ?")) {
+                $stmt->bind_param("ii", $NumberOfTickets, $EventID);
+                $stmt->execute();
+                $stmt->close();
+
+                if ($mysqli->affected_rows > 0) {
+                    // Confirmare tranzacție
+                    $mysqli->commit();
+                    echo "Achiziție realizată cu succes!";
+                } else {
+                    throw new Exception("Nu s-au putut actualiza biletele.");
+                }
+            } else {
+                throw new Exception("Nu se poate executa update pe Event.");
+            }
+        } catch (Exception $e) {
+            // Rollback în caz de eroare
+            $mysqli->rollback();
+            echo "Eroare la achiziție: " . $e->getMessage();
         }
     }
 }
 $mysqli->close();
 ?>
-
 
