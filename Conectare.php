f058d4839ff05563c92a2cfbf303203904b3e6e2
blanka <blanka@DESKTOP-1UP747C>
2023-11-22 02:44:31 -0900

    updatat

--- Conectare.php
+++ Conectare.php
@@ -1,15 +1,9 @@
 <?php
-/*** mysql hostname ***/
 $hostname = 'localhost';
-/*** mysql username ***/
 $username = 'root';
-/*** mysql password ***/
 $password = '';
-/*** baza de date ***/
 $db = 'proiect_evenimente';
-/*** se creaza un obiect mysqli ***/
 $mysqli = new mysqli($hostname, $username, $password,$db);
-/* se verifica daca s-a realizat conexiunea */
 if(!mysqli_connect_errno())
 {
 //    echo 'Connectat la baza de date: '. $db;
